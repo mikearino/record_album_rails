67aae6a954b67c74bd7ea2009d1f4987228b77d3
Student <example@example.com>
2019-08-05 10:55:26 -0700

    add functional application

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,8 +15,35 @@ class SongsController < ApplicationController
     end
   end
 
-private
+  def show
+    @album = Album.find(params[:album_id])
+    @song = Song.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @album = Album.find(params[:album_id])
+    @song = Song.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to album_path(@song.album)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to album_path(@song.album)
+  end
+
+  private
   def song_params
     params.require(:song).permit(:name, :lyrics)
   end
-end 
+end
